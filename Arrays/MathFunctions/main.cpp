0539934949936ee8f214a92e8ba4fcd8577dbf27
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-06-13 22:38:02 +0300

    MathFunctions DONE

---
+++ Arrays/MathFunctions/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+template<typename T> T Factorial(T number);
+template<typename T> T Power(T number, int power);
+
+typedef int DataType;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	DataType number;
+	int power;
+
+	cout << "¬ведите число: ";
+	cin >> number;
+	cout << "‘акториал числа: " << Factorial(number) << endl;
+
+	cout << "¬ведите показатель степени: ";
+	cin >> power;
+	cout << "„исло в степени " << power << ": " << Power(number, power) << endl;
+
+	return 0;
+}
+
+template<typename T>
+T Factorial(T number)
+{
+	T result{1};
+
+	if (number < 0) return 0;
+
+	for (int i = number; i > 0; --i) result *= i;
+
+	return result;
+}
+
+template<typename T>
+T Power(T number, int power)
+{
+	T result{ 1 };
+
+	if (power == 0) return 1;
+
+	for (int i = 0; i < power; ++i) result *= number;
+
+	return result;
+}
